f03beb2248cd210fe6212ad8f76b5bfdc171e9b9
sanjay <sanjaysulochanap@gmail.com>
2021-12-21 21:34:19 +0530

    changes done

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,36 @@
 // Write your code here.
+import './index.css'
+
+const FaqItem = props => {
+  const {faqDetails, onShowDetails} = props
+  const {id, questionText, answerText, showDetails} = faqDetails
+
+  const onClickButton = () => {
+    onShowDetails(id)
+  }
+
+  return (
+    <li className="listContainer border border-dark mb-2">
+      <div className="d-flex flex-row justify-content-between">
+        <h1 className="headingStyle">{questionText}</h1>
+        <button
+          type="button"
+          onClick={onClickButton}
+          className="bg-transparent border-0"
+        >
+          <img
+            src={
+              showDetails
+                ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png '
+                : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+            }
+            alt={showDetails ? 'minus' : 'plus'}
+          />
+        </button>
+      </div>
+      <hr />
+      {showDetails && <p className="para">{answerText}</p>}
+    </li>
+  )
+}
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,45 @@
 // Write your code here.
+import {Component} from 'react'
+import './index.css'
+import FaqItem from '../FaqItem'
+
+class Faqs extends Component {
+  constructor(props) {
+    super(props)
+    const {faqsList} = this.props
+    this.state = {faqsList}
+  }
+
+  onShowDetails = id => {
+    this.setState(prevState => ({
+      faqsList: prevState.faqsList.map(eachItem => {
+        if (id === eachItem.id) {
+          return {...eachItem, showDetails: !eachItem.showDetails}
+        }
+        return eachItem
+      }),
+    }))
+  }
+
+  render() {
+    const {faqsList} = this.state
+
+    return (
+      <div className="bgContainer p-4">
+        <div className="cardContainer d-flex flex-column align-items-center">
+          <h1 className="heading">FAQs</h1>
+          <ul className="p-4">
+            {faqsList.map(eachItem => (
+              <FaqItem
+                faqDetails={eachItem}
+                onShowDetails={this.onShowDetails}
+                key={eachItem.id}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default Faqs
